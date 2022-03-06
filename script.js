6574907b7825315ea3eafcc418c51d262bdfb50a
Skyseras <yasserelmahfoudi@gmail.com>
2022-03-06 22:39:32 +0100

    done

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const panels = document.querySelectorAll('.panel')
-panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+function makeZero(){
+    var textArea = document.querySelector('.display');
+    textArea.innerHTML = "";
+}
+function sendToDisplay(el){
+    var toCalculate = document.querySelector('.display');
+    var tempOperation = toCalculate.innerText+el;
+    toCalculate.innerText = tempOperation;
+}
+function deleteElement(){
+    var textArea = document.querySelector('.display');
+    var equation = textArea.innerText;
+    equation = (equation.slice(0,equation.length-1))
+    textArea.innerText=equation;
+    
+}
+function evaluateExpression(){
+    var textArea = document.querySelector('.display');
+    var equation = textArea.innerText;
+    var result = eval(equation);
+    textArea.innerText=result;
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
 }
