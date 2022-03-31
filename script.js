4898957d03616ab8f3a723ac6ea7052035220d61
Skyseras <yasserelmahfoudi@gmail.com>
2022-03-31 09:17:27 +0000

    cal point done

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-function makeZero(){
+function makeZero() {
     var textArea = document.querySelector('.display');
     textArea.innerHTML = "";
+    document.getElementById('point').disabled = false;
 }
-function sendToDisplay(el){
+
+function sendToDisplay(el) {
     var toCalculate = document.querySelector('.display');
-    var tempOperation = toCalculate.innerText+el;
+    var tempOperation = toCalculate.innerText + el;
     toCalculate.innerText = tempOperation;
 }
-function deleteElement(){
+
+function sendoperator(el) {
+    var toCalculate = document.querySelector('.display');
+    var tempOperation = toCalculate.innerText + el;
+    toCalculate.innerText = tempOperation;
+    document.getElementById('point').disabled = false;
+}
+
+function sendToDisplaypoint(el) {
+    var toCalculate = document.querySelector('.display');
+    var tempOperation = toCalculate.innerText + el;
+    toCalculate.innerText = tempOperation;
+    document.getElementById('point').disabled = true;
+}
+
+function deleteElement() {
     var textArea = document.querySelector('.display');
     var equation = textArea.innerText;
-    equation = (equation.slice(0,equation.length-1))
-    textArea.innerText=equation;
-    
+    equation = (equation.slice(0, equation.length - 1))
+    textArea.innerText = equation;
 }
-function evaluateExpression(){
+
+function evaluateExpression() {
     var textArea = document.querySelector('.display');
     var equation = textArea.innerText;
     var result = eval(equation);
-    textArea.innerText=result;
-
+    textArea.innerText = result;
+    document.getElementById('point').disabled = false;
 }
